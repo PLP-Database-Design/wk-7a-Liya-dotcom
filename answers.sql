30e779694065a1f41e72ad33fd1fc33a767a97e2
Liya-dotcom <simethebe@gmail.com>
2025-04-23 20:18:29 +0200

    Week 7 Assignment Submitted

---
+++ answers.sql
@@ -0,0 +1,46 @@
+-- QUESTION 1
+-- Inerting values that will meet the criteria of the 1NF
+INSERT INTO ProductDetail (OrderID, CustomerName, Product)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smoith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+
+-- QUESTION 2
+-- Transforming OrderDetails table into 2NF
+-- 
+-- Create Orders table
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(255)
+);
+
+-- Insert data into Orders table
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+    (101, 'John Doe'),
+    (102, 'Jane Smith'),
+    (103, 'Emily Clark');
+
+-- Create OrderProducts table
+CREATE TABLE OrderProducts (
+    OrderID INT,
+    Product VARCHAR(255),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+-- Insert data into OrderProducts table
+INSERT INTO OrderProducts (OrderID, Product, Quantity)
+VALUES
+    (101, 'Laptop', 2),
+    (101, 'Mouse', 1),
+    (102, 'Tablet', 3),
+    (102, 'Keyboard', 1),
+    (102, 'Mouse', 2),
+    (103, 'Phone', 1);
